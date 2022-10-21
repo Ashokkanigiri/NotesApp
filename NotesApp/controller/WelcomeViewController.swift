384a6276f701d888164436e799a7f1feb49bb21e
Ashokkanigiri <ashkanigiri@gmail.com>
2022-10-21 12:02:58 +0530

    1. added click handlers in welcome controller

---
+++ NotesApp/controller/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Ashok Kanigiri on 21/10/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBAction func registerClickHandler(_ sender: UIButton) {
+        
+    }
+    @IBAction func loginClickHandler(_ sender: UIButton) {
+        
+    }
+    @IBAction func loginWithGmailClickHandler(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
