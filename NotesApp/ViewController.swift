f5037a97a48c5363206d432ba055886d1ae18bea
Ashokkanigiri <ashkanigiri@gmail.com>
2022-10-21 11:43:42 +0530

    project setup

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Ashok Kanigiri on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
